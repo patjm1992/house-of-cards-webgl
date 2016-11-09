f2befac7a7db015308412bd581dbf6ae732865af
patjm1992 <patjm1992@gmail.com>
2016-11-09 15:40:51 -0500

    ui

--- fx.js
+++ fx.js
@@ -10,20 +10,24 @@
 });
 
 */
+$(document).ready(function() {
 
-$('#first, #second, #third, #fourth, #fifth, #sixth, #seventh, #eighth, #ninth, #tenth, #eleventh').letterFade({
-    fade: 'in',   // fade letters 'in' or 'out'
-    duration: 100,  // how long the fade lasts per-letter
-    delay: 0  // how long to wait between letters
-});
+	$('#first, #second, #third, #fourth, #fifth, #sixth, #seventh, #eighth, #ninth, #tenth, #eleventh').letterFade({
+	    fade: 'in',   // fade letters 'in' or 'out'
+	    duration: 25,  // how long the fade lasts per-letter
+	    delay: -10  // how long to wait between letters
+	});
 
-$("html").click(function() {
-	var speed = 1000;
-
-	$("#second").fadeIn(speed);
-	$("#third").fadeIn(speed * 2);
-	$("#fourth").fadeIn(speed * 3);
-	$("#fifth").fadeIn(speed * 4);
-	$("#sixth").fadeIn(speed * 5);
-	$("#seventh").fadeIn(speed * 6);
+	$("html").click(function () {
+		speed = 1000;
+		$("#second").fadeOut(speed);
+		$("#third").fadeOut(speed);
+		$("#fourth").fadeOut(speed);
+		$("#fifth").fadeOut(speed);
+		$("#sixth").fadeOut(speed);
+		$("#seventh").fadeOut(speed);
+    	$("#eighth").fadeOut(speed);
+		$("#ninth").fadeOut(speed);
+				$("#first").fadeOut(speed);
+	})
 });
